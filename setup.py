89e83a611df955e84a274eddb27d7a21813bce1d
Christopher Engelhard <ce@lcts.de>
2020-07-22 18:35:42 +0200

    add mail address

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-# This file is part of hello-copr <https://pagure.io/copr-tito-quickdoc>.
+# This file is part of hellocopr <https://pagure.io/copr-tito-quickdoc>.
 # Copyright (C) 2020 Christopher Engelhard
 #
-# hello-copr is free software: you can redistribute it and/or modify
+# hellocopr is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# hello-copr is distributed in the hope that it will be useful,
+# hellocopr is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with hello-copr.  If not, see <http://www.gnu.org/licenses/>.
+# along with hellocopr.  If not, see <http://www.gnu.org/licenses/>.
 
+import codecs, os.path
 from setuptools import setup, find_packages
 
 # use README.md as readme
@@ -29,6 +30,7 @@ setup(
     long_description_content_type="text/markdown",
     url='https://pagure.io/copr-tito-quickdoc',
     author='Christopher Engelhard',
+    author_email='ce@lcts.de',
     license='GPLv3',
     version='1.0.0',
 
